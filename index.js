e748b7d17432dcde0ff6be2b447c428d7a093413
Grigore-Mihai-Alexandru <mihaigrigore586@gmail.com>
2024-09-19 20:02:31 +0300

    cloud-vision-working

--- index.js
+++ index.js
@@ -1,22 +1,37 @@
 const vision = require('@google-cloud/vision');
+const fs = require("fs");
 
-console.log("ok")
 
 async function analyzeImage (image){
     try{
         const client = new vision.ImageAnnotatorClient({
-            keyFilename:"../../../key.json"   //change path(add file)
+            keyFilename:"../../../keyy.json"   //change path(add file)
         });
-        console.log("ok")
+
         // example of a specific thing to look for:
         const labeldetection = await client.labelDetection(image); 
-        return {labeldetection};
+        const logoDetection = await client.logoDetection(image); 
+        const imageProps = await client.imageProperties(image);
+        return {labeldetection, logoDetection, imageProps};
     }catch(error){
         console.error("error:",error);
     }
 
 } 
-(async () => {
+async function saveDataToFile(data) {
+    try {
+      const jsonData = JSON.stringify(data, null, 2); // The 'null, 2' adds indentation to make the file more readable
+  
+      fs.writeFileSync('data.json', jsonData);
+  
+    } catch (error) {
+      console.error('Error writing data to file:', error);
+    }
+  }
+async function start (){
     const imageAn = await analyzeImage("images.png")
-    console.log(imageAn);
-} )
+    
+    saveDataToFile(imageAn);
+}
+
+start();
